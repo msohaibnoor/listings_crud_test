04fca81697b0717f193c670e7d740cd5733aee3a
Arik Hassan <arik-h@hotmail.com>
2024-09-26 21:48:35 -0400

    Debugging

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.post('/api/listings', async(req, res) =>{
 
 // GET route /api/listings
   app.get("/api/listings", (req,res) => {
+    res.send("request for /api/listings");
     db.getAllListings(req.query.page, req.query.perPage)   
         .then((listings) => {
             res.status(200).json(listings);
