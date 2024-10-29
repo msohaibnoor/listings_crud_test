1a28a5267a12e108e6c260552da7e380a417f158
msohaibnoor <sohaibfarrukh59@gmail.com>
2024-10-30 00:04:57 +0500

    db connection

--- api/listings/[id].js
+++ api/listings/[id].js
@@ -1,7 +1,21 @@
 // /api/listings/[id].js
 const ListingsDB = require("../../modules/listingsDB.js");
 const db = new ListingsDB();
-db.initialize(process.env.MONGODB_CONN_STRING);
+// db.initialize(process.env.MONGODB_CONN_STRING);
+
+db.initialize(process.env.MONGODB_CONN_STRING)
+  .then(() => {
+    console.log("Database connection successful.");
+
+    // Start the server only after the database connection is established
+    app.listen(HTTP_PORT, () => {
+      console.log(`Server is running on port ${HTTP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+    process.exit(1); // Exit the app if the database connection fails
+  });
 
 module.exports = async (req, res) => {
   const { id } = req.query;
@@ -22,12 +36,10 @@ module.exports = async (req, res) => {
   } else if (req.method === "PUT") {
     try {
       const updatedListing = await db.updateListingById(req.body, id);
-      res
-        .status(200)
-        .json({
-          message: `Listing updated successfully`,
-          result: updatedListing,
-        });
+      res.status(200).json({
+        message: `Listing updated successfully`,
+        result: updatedListing,
+      });
     } catch (error) {
       res
         .status(500)
