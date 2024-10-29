e7f855f57e01dec871dc4234ad5a4cf3a590ddc3
msohaibnoor <sohaibfarrukh59@gmail.com>
2024-10-29 21:37:53 +0500

    add api folder

---
+++ api/listings.js
@@ -0,0 +1,14 @@
+const ListingsDB = require("../modules/listingsDB");
+const db = new ListingsDB();
+
+module.exports = async (req, res) => {
+  await db.initialize(process.env.MONGODB_CONN_STRING);
+  const { page = 1, perPage = 10, name = "" } = req.query;
+
+  try {
+    const listings = await db.getAllListings(page, perPage, name);
+    res.status(200).json(listings);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
